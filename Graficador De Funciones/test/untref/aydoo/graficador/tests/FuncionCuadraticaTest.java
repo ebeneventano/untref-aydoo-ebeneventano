b0c994bf5353b9620c49146fb64360d5cbabde08
ebeneventano <emmanuel.beneventano@gmail.com>
2014-04-26 18:53:02 -0300

    Funcion Cuadratica Test

--- Graficador De Funciones/test/untref/aydoo/graficador/tests/FuncionCuadraticaTest.java
+++ Graficador De Funciones/test/untref/aydoo/graficador/tests/FuncionCuadraticaTest.java
@@ -3,7 +3,38 @@ public class FuncionCuadraticaTest {
 	
 	@Test
 	public void evualuarEn0DeberiaDarC(){
-		
+		float a = 2.0f;
+		float b = 3.0f;
+		float c = 4.0f
+		float x = 0.0f;
+		float resultado = c;
+
+		FuncionCuadratica funcionCuadratica = new FuncionCuadratica(a, b, c);
+		Assert.assertEquals(resultado, funcionCuadratica.evaluarEn(x), 0.0001);
+	}
+	
+	@Test
+	public void evualuarEn1DeberiaDar9CuandoAIgual2BIgual3yCIgual4(){
+		float a = 2.0f;
+		float b = 3.0f;
+		float c = 4.0f
+		float x = 1.0f;
+		float resultado = 9.0f;
+
+		FuncionCuadratica funcionCuadratica = new FuncionCuadratica(a, b, c);
+		Assert.assertEquals(resultado, funcionCuadratica.evaluarEn(x), 0.0001);
+	}
+	
+	@Test
+	public void evualuarEnMenos1DeberiaDar3CuandoAIgual2BIgual3yCIgual4(){
+		float a = 2.0f;
+		float b = 3.0f;
+		float c = 4.0f
+		float x = -1.0f;
+		float resultado = 3.0f;
+
+		FuncionCuadratica funcionCuadratica = new FuncionCuadratica(a, b, c);
+		Assert.assertEquals(resultado, funcionCuadratica.evaluarEn(x), 0.0001);
 	}
 
 }
